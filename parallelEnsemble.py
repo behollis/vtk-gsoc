dae858d913115ee025b693cb63669b669da40eed
Brad Eric Hollister <behollis@soe.ucsc.edu>
2014-07-25 16:14:45 -0700

    changed dir structure / streamline parameters, one st per vtp file organized in file sys

--- parallelEnsemble.py
+++ parallelEnsemble.py
@@ -41,15 +41,14 @@ class EnsembleReader(vta.VTKAlgorithm):
         
         return 1
 
-# mpiexec -n <NUM_PROCESSES> pvtkpython parallelEnsemble.py
+# mpiexec -n <NUM_CORES> pvtkpython parallelEnsemble.py
 
-NUM_CORES = 12#16#8
-NUM_PROCS = 1
-MEMBERS = NUM_CORES * 83#62#125
-SLICE = MEMBERS / (NUM_CORES * NUM_PROCS) #division of members per thread
+NUM_CORES = 12
+MEMBERS = NUM_CORES * 83
+SLICE = MEMBERS / NUM_CORES #division of members per thread
 EXT_X = 127
 EXT_Y = 127
-ROOT = '../../../'
+ROOT = '/home/behollis/'
 
 if __name__ == '__main__':
     
@@ -60,35 +59,9 @@ if __name__ == '__main__':
         if grank == 0:
             print 'Only an even number of ranks is support'
         sys.exit(0)
-    
-    # Split the process space to 2 (because there are 2
-    # ensemble members. Can be generalized to n members)
-    # Note that if localSize > 1, there will be redundant
-    # IO because of parallelization over seeds.
-
-    #print 'gsize ' + str(gsize)
-    #print 'grank ' + str(grank)
+   
     localSize = gsize / NUM_CORES
-    
-    #print 'localsize ' + str(localSize)
     localGroup = grank / localSize
-    
-    contr = None
-    
-    '''
-    for i in range(MEMBERS):
-        group = vtk.vtkProcessGroup()
-        group.SetCommunicator(wc.GetCommunicator())
-        for j in range(localSize):
-            group.AddProcessId(i*localSize + j)
-        c = wc.CreateSubController(group)
-        if c != None:
-            contr = c
-    
-    rank = contr.GetLocalProcessId()
-    print 'rank: ' + str(rank)
-    size = contr.GetNumberOfProcesses()
-    '''
     
     r = vtk.vtkEnsembleSource()
     
@@ -98,7 +71,6 @@ if __name__ == '__main__':
     aColumn = vtk.vtkIntArray()
     aColumn.SetName("Ensemble Index")
    
-    
     for mem in range(1,MEMBERS+1):
         # Add source / reader
         ps = vtk.vtkPythonAlgorithm()
@@ -113,113 +85,64 @@ if __name__ == '__main__':
     outInfo = r.GetOutputInformation(0)
     outInfo.Set(vtk.vtkEnsembleSource.UPDATE_MEMBER(), localGroup)
     r.Update()
-    
-    #terninal points over field in a member
-    #fpts_x = numpy.zeros(shape=(EXT_X, EXT_Y))
-    #fpts_y = numpy.zeros(shape=(EXT_X, EXT_Y))
-    
+   
     st = vtk.vtkStreamTracer()
-    
     st.SetController(vtk.vtkDummyController())
     st.SetInputArrayToProcess(0, 0, 0, vtk.vtkDataObject.FIELD_ASSOCIATION_POINTS, "velocity")
     st.SetInputData(r.GetOutputDataObject(0))
     
-    # seed points for half of grid
-    grid1 = vtk.vtkUnstructuredGrid()
-    pts1 = vtk.vtkPoints()
-    
-    for x in range( 0, EXT_X-8, 4 ):
-        for y in range( 0, EXT_Y-8, 4 ):
-            pts1.InsertNextPoint(float(x),float(y),0.)
-    
-    # seed points for other half of grid
-    grid2 = vtk.vtkUnstructuredGrid()
-    pts2 = vtk.vtkPoints()
-    
-    for x in range( 3, EXT_X-8, 4 ):
-        for y in range( 1, EXT_Y-8, 4 ):
-            pts2.InsertNextPoint(float(x),float(y),0.)
-            
-   
-    grid1.SetPoints(pts1)
-    grid2.SetPoints(pts2)
+    '''
+    grid = vtk.vtkUnstructuredGrid()
+    pts = vtk.vtkPoints()
+    pts.InsertPoint(float(x),float(y),0.)   
+    grid.SetPoints(pts)
+    '''
    
     #st.SetSourceConnection( pt.GetOutputPort() )
     st.SetMaximumPropagation(5000)
-    st.SetMaximumNumberOfSteps(500) 
-    st.SetInitialIntegrationStep(0.5)
+    st.SetMaximumNumberOfSteps(1000) 
+    st.SetInitialIntegrationStep(0.2)
     st.SetMinimumIntegrationStep(0.1)
-    st.SetMaximumIntegrationStep(0.5)
+    st.SetMaximumIntegrationStep(0.2)
     st.SetTerminalSpeed(0.000001)
     st.SetIntegratorTypeToRungeKutta45()
+    st.SetIntegrationDirectionToBoth()
     
-    dir = ROOT+'lockExchangeStreamlinesTs0050/'+str(grank*SLICE)+'/'
+    dir = ROOT+'lockExSt/ts00050/'
     if not os.path.exists(dir):
         os.makedirs(dir)
-   
+        
     for mem in range( grank*SLICE, (grank+1)*SLICE ):
-        # write out streamlines for this member(s)
-        w = vtk.vtkXMLPolyDataWriter()
-        
-        mem_num = str(mem).zfill(4)
-        print 'mem: ' + mem_num
-        
-        #1F
-        w.SetFileName(dir+'1Fslines%s.vtp' % mem_num)
-        st.SetSourceData(grid1)  
-        st.SetIntegrationDirectionToForward()
-        st.Update()
-        
-        w.SetInputConnection(st.GetOutputPort())
-        w.Update()
-        w.Write()
-        
-        #1B
-        w.SetFileName(dir+'1Bslines%s.vtp' % mem_num)
-        st.SetSourceData(grid1)  
-        st.SetIntegrationDirectionToBackward()
-        st.Update()
-        
-        w.SetInputConnection(st.GetOutputPort())
-        w.Update()
-        w.Write()
-        
-        #2F
-        w.SetFileName(dir+'2Fslines%s.vtp' % mem_num)
-        st.SetSourceData(grid2)
-        st.SetIntegrationDirectionToForward()
-        st.Update()
-        
-        w.SetInputConnection(st.GetOutputPort())
-        w.Update()
-        w.Write()
-        
-        #2B
-        w.SetFileName(dir+'2Bslines%s.vtp' % mem_num)
-        st.SetSourceData(grid2)
-        st.SetIntegrationDirectionToBackward()
-        st.Update()
-        
-        w.SetInputConnection(st.GetOutputPort())
-        w.Update()
-        w.Write()
-        
-        
-       
-        
-        
-        
-        '''    
-        line = st.GetOutput().NewInstance()
-        line.ShallowCopy(st.GetOutput())
+        print 'mem: ' + str(mem).zfill(4)
+        for x in range(0,EXT_X):
+            
+            sdir = dir + str(mem).zfill(4) + '/x' + str(x).zfill(3) 
+            if not os.path.exists(sdir):
+                os.makedirs(sdir)
+            
+            os.chdir(sdir)
+            
+            for y in range(0,EXT_Y):
+                
+                ssdir = sdir + '/y' + str(y).zfill(3) 
+                if not os.path.exists(ssdir):
+                    os.makedirs(ssdir)
+            
+                os.chdir(ssdir)
     
-        tpt = line.GetPoint(line.GetNumberOfPoints()-1)
-        
-        fpts_x[x][y] = tpt[0]
-        fpts_y[x][y] = tpt[1]
-        '''
-        
-        #numpy.savetxt('./out/x_member_%d.txt' % grank, fpts_x)
-        #numpy.savetxt('./out/y_member_%d.txt' % grank, fpts_y)
-        
-       
+                w = vtk.vtkXMLPolyDataWriter()
+                grid = vtk.vtkUnstructuredGrid()
+                pts = vtk.vtkPoints()
+            
+                pts.InsertNextPoint(float(x),float(y),0.)   
+                grid.SetPoints(pts)
+                
+                w.SetFileName('sline_M' + str(mem).zfill(4) + '_X' + str(x).zfill(3) + '_Y' + str(y).zfill(3) +'.vtp')
+                st.SetSourceData(grid)  
+                st.Update()
+    
+                w.SetInputConnection(st.GetOutputPort())
+                w.Update()
+                w.Write()
+                
+                os.chdir('..')
